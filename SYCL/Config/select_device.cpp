3f076c982bd6b0d20bf519690de1ecb269349380
sami_hatna66 <sami.hatna2003@gmail.com>
2022-07-08 09:24:11 +0100

    WRITE_REG_EX_INFO now passes for CUDA and HIP backends

--- SYCL/Config/select_device.cpp
+++ SYCL/Config/select_device.cpp
@@ -474,25 +474,36 @@ int main() {
               addEscapeSymbolToSpecialCharacters(name);
               std::string ver = dev.get_info<info::device::driver_version>();
               size_t pos = 0;
-              if ((pos = ver.find(".")) == std::string::npos) {
-                throw std::runtime_error("Malformed syntax in version string");
-              }
-              pos++;
-              size_t start = pos;
-              if ((pos = ver.find(".", pos)) == std::string::npos) {
-                throw std::runtime_error("Malformed syntax in version string");
-              }
-              ver.replace(start, pos - start, "*");
               if (((plt.get_backend() == backend::opencl) &&
                    (sycl_be.find("opencl") != std::string::npos)) ||
-                  (((plt.get_backend() == backend::ext_oneapi_level_zero) ||
-                    (plt.get_backend() == backend::level_zero)) &&
+                  ((plt.get_backend() == backend::ext_oneapi_level_zero) &&
                    (sycl_be.find("level_zero") != std::string::npos))) {
-                fs << "DeviceName:{{" << name << "}},DriverVersion:{{" << ver
-                   << "}}" << std::endl;
-                passed = true;
-                break;
+                if ((pos = ver.find(".")) == std::string::npos) {
+                  throw std::runtime_error("Malformed syntax in version string");
+                }
+                pos++;
+                size_t start = pos;
+                if ((pos = ver.find(".", pos)) == std::string::npos) {
+                  throw std::runtime_error("Malformed syntax in version string");
+                }
+                ver.replace(start, pos - start, "*");
+              } else if (((plt.get_backend() == backend::cuda) || 
+                          (plt.get_backend() == backend::ext_oneapi_cuda)) && 
+                         (sycl_be.find("cuda") != std::string::npos) ||
+                         ((plt.get_backend() == backend::hip) ||
+                          (plt.get_backend() == backend::ext_oneapi_hip)) &&
+                         (sycl_be.find("hip") != std::string::npos)) {
+                if ((pos = ver.find(".")) == std::string::npos) {
+                  throw std::runtime_error("Malformed syntax in version string");
+                }
+                pos++;
+                ver.replace(pos, ver.length(), "*");
               }
+
+              fs << "DeviceName:{{" << name << "}},DriverVersion:{{" << ver
+                 << "}}" << std::endl;
+              passed = true;
+              break;
             }
           }
         }
